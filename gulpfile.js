38c47a7449af9591a08bc28b18007c4184756ac8
Arnun <it_531413016@hotmail.com>
2017-01-11 12:20:25 +0700

    little change structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
-var gulp = require('gulp'),
-    pug = require('gulp-pug2'),
-    watch = require('gulp-watch'),
-    sass = require('gulp-sass'),
-    cleanCSS = require('gulp-clean-css');
+var gulp = require('gulp'),             //core
+    pug = require('gulp-pug2'),         //pug -> HTML
+    watch = require('gulp-watch'),      //watch file change and trigger
+    sass = require('gulp-sass'),        //scss -> CSS
+    cleanCSS = require('gulp-clean-css'),   //minify css
+    strip = require('gulp-strip-comments'); //strip comments from files
 //var compass = require('gulp-compass');
-
-//---new Watch---//
 
 var path_new = {
     source: ['./src/html/**/*.pug', '!./src/html/include/**/*.pug'],
@@ -19,6 +18,7 @@ gulp.task('default', function () {
 gulp.task('views', function buildHTML() {
     return gulp.src(path_new.source)
         .pipe(pug())
+        .pipe(strip())
         .pipe(gulp.dest(path_new.dest))
 });
 gulp.task('scss', function buildHTML() {
